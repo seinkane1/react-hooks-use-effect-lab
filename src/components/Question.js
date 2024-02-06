9efe21f853171985454f20794bdf25825888a6c5
sein <seinkane01@gmail.com>
2024-02-06 21:26:01 +0300

    completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
+import questions from "../data/quiz";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(prevTime => prevTime - 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10); 
+      onAnswered(false); 
+    }
+  }, [timeRemaining, onAnswered]);
+
+  return (
+    <div>
+      <p>Time Remaining: {timeRemaining}</p>
+    
+    </div>
+  );
+};
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  const { id, prompt, answers, correctIndex } = Question;
 
-  return (
+
+
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
@@ -22,11 +47,12 @@ function Question({ question, onAnswered }) {
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
+        
         );
       })}
       <h5>{timeRemaining} seconds remaining</h5>
     </>
-  );
-}
+  ;
+
 
 export default Question;
